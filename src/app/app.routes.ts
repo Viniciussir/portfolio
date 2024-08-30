3f73b4f569861f0a28e572cc34dfc9f1eda5da44
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-08-30 10:41:09 -0300

    Adicionado os componentes e paginas iniciais do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        redirectTo: '/home', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'home',
+        component: HomeComponent 
+    },
+    { 
+        path: 'about', 
+        component: AboutComponent 
+    },
+    { 
+        path: 'experience', 
+        component: ExperienceComponent 
+    },
+    { 
+        path: 'projects', 
+        component: ProjectsComponent 
+    },
+    { 
+        path: 'contact', 
+        component: ContactComponent 
+    },
+    {
+        path: '**',
+        component: HomeComponent
+    },
+  ];
