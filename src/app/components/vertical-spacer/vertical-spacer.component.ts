559fc67edafc4e5c044a822273f6d572e7516a93
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-09-30 14:18:45 -0300

    Esboço da section de projetos e ajustes no separador

--- src/app/components/vertical-spacer/vertical-spacer.component.ts
+++ src/app/components/vertical-spacer/vertical-spacer.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vertical-spacer',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './vertical-spacer.component.html',
   styleUrl: './vertical-spacer.component.css'
 })
 export class VerticalSpacerComponent {
   @Input() verticaltext: string = ''; 
+  @Input() firstColorGradient: string = ''; 
+  @Input() secondColorGradient: string = ''; 
 }
