aa985f80a01b461c2d63736f70361b3401a90f14
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-10-02 11:36:30 -0300

    Ajuste na estrutura do about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SkillBoxComponent } from '../../components/skill-box/skill-box.component';
 import { StructuredBoxComponent } from "../../components/structured-box/structured-box.component";
+import { VerticalSpacerComponent } from '../../components/vertical-spacer/vertical-spacer.component';
 
 @Component({
   selector: 'app-about',
@@ -9,7 +10,8 @@ import { StructuredBoxComponent } from "../../components/structured-box/structur
   imports: [
     CommonModule,
     SkillBoxComponent,
-    StructuredBoxComponent
+    StructuredBoxComponent,
+    VerticalSpacerComponent
 ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
@@ -27,4 +29,7 @@ export class AboutComponent {
 
   infoTextFour: string = 'Português Nativo | Inglês  Intermediario';
   verticalTextFour: string = '4';
+
+  infoTextFive: string = 'Soft skills';
+  verticalTextFive: string = '5';
 }
