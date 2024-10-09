65eabe9e1221003333c9013f238b55796530e50a
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-10-09 11:44:02 -0300

    Criando o componente standalone de contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,33 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ContactFormComponent } from "../../components/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+  imports: [ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
-  contactForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.contactForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', [Validators.required]]
-    });
-  }
-
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log('Form data:', this.contactForm.value);
-    }
-  }
 
 }
