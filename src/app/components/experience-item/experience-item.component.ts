fdf88cecd9304444e57715ef95409fa01b888dc3
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-10-13 17:51:54 -0300

    Criação do componente para listar o item de experiencia

---
+++ src/app/components/experience-item/experience-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience-item.component.html',
+  styleUrl: './experience-item.component.css'
+})
+export class ExperienceItemComponent {
+
+}
