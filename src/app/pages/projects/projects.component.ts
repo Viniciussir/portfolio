c1894fdf29893ec45b9d167a8f7f079caa16d7ac
Vinicius C Scampini <viniciuscscampini@gmail.com>
2024-10-14 17:16:54 -0300

    Adicionando referencia ao projeto de pokeapi

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -16,11 +16,23 @@ import { ProjectInfoComponent } from '../../components/project-info/project-info
 })
 export class ProjectsComponent {
 
-  projectTitle1:string = 'Portfólio';
-  projectDescription1:string = 'Este é meu portfólio pessoal, criado em Angular para destacar minhas habilidades e projetos. O design responsivo proporciona uma experiência fluida em diversos dispositivos. Usei CSS puro e SCSS para um layout moderno e organizado, incluindo links para meus repositórios no GitHub e deploy via GitHub Pages.';
-  deployLink1:string = 'https://viniciussir.github.io/portfolio/';
-  githubLink1:string = 'https://github.com/Viniciussir/portfolio';
-  tags1:{ name: string; color: string }[] = [
+  projectPortfolio:string = 'Portfólio';
+  projectPortfolioDescription:string = 'Este é meu portfólio pessoal, criado em Angular para destacar minhas habilidades e projetos. O design responsivo proporciona uma experiência fluida em diversos dispositivos. Usei CSS puro e SCSS para um layout moderno e organizado, incluindo links para meus repositórios no GitHub e deploy via GitHub Pages.';
+  projectPortfolioDeployLink:string = 'https://viniciussir.github.io/portfolio/';
+  projectPortfolioGithubLink:string = 'https://github.com/Viniciussir/portfolio';
+  projectPortfolioTags:{ name: string; color: string }[] = [
+    { name: 'Angular', color: '#dd0031' },
+    { name: 'TypeScript', color: '#3178c6' },
+    { name: 'HTML', color: '#3178c6' },
+    { name: 'CSS', color: '#c6538c' },
+    { name: 'SCSS ', color: '#c6538c' }
+  ]
+
+  projectPokeapi:string = 'Pokeapi';
+  projectPokeapiDescription:string = 'Este projeto é uma aplicação em Angular que utiliza a PokeAPI para explorar o universo Pokémon. Com design responsivo, uso de CSS puro e SCSS, a interface é moderna e intuitiva. Os usuários podem buscar e visualizar Pokémon, com detalhes sobre tipos e habilidades. O projeto está hospedado no GitHub Pages, com links para o repositório.';
+  projectPokeapiDeployLink:string = 'https://viniciussir.github.io/pokeapi/';
+  projectPokeapiGithubLink:string = 'https://github.com/Viniciussir/pokeapi';
+  projectPokeapiTags:{ name: string; color: string }[] = [
     { name: 'Angular', color: '#dd0031' },
     { name: 'TypeScript', color: '#3178c6' },
     { name: 'HTML', color: '#3178c6' },
